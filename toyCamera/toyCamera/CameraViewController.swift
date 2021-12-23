da7aeeeb0eb7090586ab62f164c9bd157a55d5f8
mandeuk26 <beddl0626@gmail.com>
2021-12-23 15:36:51 +0900

    feat: StoryBoard 제거

---
+++ toyCamera/toyCamera/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toyCamera
+//
+//  Created by 강현준 on 2021/12/23.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
