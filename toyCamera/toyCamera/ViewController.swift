4db01f6ddd80a97f9ea3534186a80cad2c40441b
mandeuk26 <beddl0626@gmail.com>
2021-12-23 15:25:04 +0900

    feat: 프로젝트 생성

---
+++ toyCamera/toyCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toyCamera
+//
+//  Created by 강현준 on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
